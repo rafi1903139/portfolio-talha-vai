1ade8c06bdd8c9f33c4e3faf63b8a7a9e644ef32
rafi1903139 <rafsan.rafi159@gmail.com>
2022-09-28 14:25:11 +0600

    link modified

--- common/scripts/utilities.js
+++ common/scripts/utilities.js
@@ -1,37 +1,141 @@
-function includeHTML()
+var navForIndex = `
+<!--nav start-->
+
+<nav
+class="
+  navbar navbar-expand-lg navbar-light
+  position-sticky
+  top-0
+  fs-5
+  pb-0
+"
+>
+<div class="container-fluid">
+  <a class="navbar-brand fs-5" href="./index.html"
+    ><img src="./assets/logo/logo.png" alt="logo"
+  /></a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div
+    class="collapse navbar-collapse justify-content-end"
+    id="navbarNavAltMarkup"
+  >
+    <div class="navbar-nav p-2">
+      <a
+        class="nav-text nav-link active underline"
+        aria-current="page"
+        href="../../index.html"
+        >Home</a
+      >
+      <a
+        class="nav-text nav-link px-3"
+        href="./views/experience/experience.html"
+        >Experience</a
+      >
+      <a class="nav-link px-3" href="./views/education/education.html">Education</a>
+      <a class="nav-link px-3" href="./views/skills/skills.html">Skills</a>
+      <a class="nav-link px-3" href="./views/trainings/trainings.html"
+        >Training</a
+      >
+      <a class="nav-link px-3" href="./views/portfolio/portfolio.html">Portfolio</a>
+      <a class="nav-link px-3" href="#">Extra Curricular</a>
+      <a class="nav-link px-3" href="./views/reference/refrence.html">Reference</a>
+      <a class="nav-link px-3" href="./views/organization/organization.html"
+        >Org. Affiliation</a
+      >
+    </div>
+  </div>
+</div>
+</nav>
+
+<!--nav end-->
+`;
+
+var navForOther = `
+<!--nav start-->
+
+<nav
+class="
+  navbar navbar-expand-lg navbar-light
+  position-sticky
+  top-0
+  fs-5
+  pb-0
+"
+>
+<div class="container-fluid">
+  <a class="navbar-brand fs-5" href="./index.html"
+    ><img src="../../assets/logo/logo.png" alt="logo"
+  /></a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div
+    class="collapse navbar-collapse justify-content-end"
+    id="navbarNavAltMarkup"
+  >
+    <div class="navbar-nav p-2">
+      <a id="home"
+        class="nav-text nav-link"
+        aria-current="page"
+        href="../../index.html"
+        >Home</a
+      >
+      <a  class="nav-text nav-link px-3" href="../../views/experience/experience.html">Experience</a>
+      <a  class="nav-link px-3" href="../../views/education/education.html">Education</a>
+      <a  class="nav-link px-3" href="../../views/skills/skills.html">Skills</a>
+      <a  class="nav-link px-3" href="../../views/trainings/trainings.html">Trainings</a>
+      <a  class="nav-link px-3" href="../../views/portfolio/portfolio.html">Portfolio</a>
+      <a  class="nav-link px-3" href="#">Extra Curricular</a>
+      <a class="nav-link px-3" href="../../views/reference/refrence.html">Reference</a>
+      <a class="nav-link px-3" href="../../views/organization/organization.html"
+        >Organization</a
+      >
+    </div>
+  </div>
+</div>
+</nav>
+
+<!--nav end-->
+`;
+
+var elmnt = document.querySelector('header');
+var title = document.title;
+
+if(document.title == 'Home')
 {
-  var elmnt = document.querySelector('header');
-  var file = elmnt.getAttribute('include-html');
-
-  if(file)
-  {
-    // make an http request
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4)
-        {
-            if(this.status == 200)
-            {
-                elmnt.innerHTML = this.responseText;
-            }
-            if(this.status == 404)
-            {
-                elmnt.innerHTML = "file not found";
-            }
-
-            // remove the attribute and call the function once more
-            elmnt.removeAttribute("include-html");
-            includeHTML();
-        }
-    }
-    xhttp.open("GET", file, true);
-    xhttp.send();
-
-    return;
-
-  }
-
-  
+  elmnt.innerHTML = navForIndex;
+}
+else 
+{
+  elmnt.innerHTML = navForOther;
 }
 
-includeHTML();
+var navItems = elmnt.getElementsByTagName('a');
+
+for(var item of navItems)
+{
+  console.log(item.innerHTML, title);
+  if(title.toLowerCase() == item.innerHTML.toLowerCase())
+  {
+    item.classList.add('active');
+    item.classList.add('underline');
+  }
+}
